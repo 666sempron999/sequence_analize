4c12d6fd43cb8523d4adc1de21c776c265c591b5
Yaroslav Rushechnicov <yrushechnicov@activewizards.com>
2018-11-24 19:32:30 +0300

    Full function and test interval

--- cardio1.py
+++ cardio1.py
@@ -11,7 +11,7 @@ def print_value(nValue):
         print(nValue[i])
 
 
-def show_graphic(signalData):
+def get_grafic_parametr(signalData):
     """
     """
     # Получение положительных пиков и их параметров 
@@ -20,10 +20,22 @@ def show_graphic(signalData):
     #Получение отрицательных пиков и их параметров
     negativePeaks, negativeProperties = find_peaks(-signalData, width=1)
 
+    return positivePeaks, positiveProperties, negativePeaks, negativeProperties
+
+
+
+def show_graphic(positivePeaks, positiveProperties, negativePeaks, negativeProperties, header):
+    """
+    """
+
     # Определение заголовка окна рабочей области
     fig = plt.figure(0)
-    fig.canvas.set_window_title("Анализ графиков на минимум и максимум")
+    fig.canvas.set_window_title("Анализ графиков на минимум и максимум участака {}".format(header))
 
+    # Список с цветами для закраски категорий графика
+    colorList = ["silver", "darksalmon", "gold", "indigo", "cornflowerblue"]
+
+    #--------------------------------------------------------------------------
     # Параметры для первой части графика
     plt.subplot(211).set_title("Анализ на максимумы")
     plt.grid(True, which="major", color="k", linestyle="dashed", alpha=0.5)
@@ -37,11 +49,51 @@ def show_graphic(signalData):
                xmax=positiveProperties["right_ips"], color = "C1")
 
     # Прорисовка интервалов
+    
+    right = positiveProperties.get("right_ips")
+    left = positiveProperties.get("left_ips")
 
-    #=============================================================================
+    widthsParametr = positiveProperties.get("widths")
+    prominencesParametr = positiveProperties.get("prominences")
+    intervals = list(zip(left, right))
+    intervalParemetrs = list(zip(prominencesParametr, widthsParametr))
+
+    noiceIndex = []
+    firstGroup = []
+    secondGroup = []
+    thirdGroup = []
+    fourthGroup = []
+
+    for i in range(0, len(intervalParemetrs)):
+        if intervalParemetrs[i][0] <=0.025 and intervalParemetrs[i][1] <= 2:
+            noiceIndex.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[0], alpha=1, label="Потенциальный шум (h<=0.025; w<=2)" if len(noiceIndex) == 1 else "")
+
+        elif intervalParemetrs[i][0] <=0.025 and (intervalParemetrs[i][1] > 2 and intervalParemetrs[i][1] <= 3):
+            firstGroup.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[1], alpha=0.75, label="1-я группа (h<=0.025; w(2-3))" if len(firstGroup) == 1 else "")
+
+        elif ((intervalParemetrs[i][0] <=0.025) and (intervalParemetrs[i][1] > 3 and intervalParemetrs[i][1] <= 7)):
+            secondGroup.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[2], alpha=0.5, label="2-я группа (h<=0.025; w(3-7)" if len(secondGroup) == 1 else "")
+
+        elif ((intervalParemetrs[i][0] <=0.3) and (intervalParemetrs[i][1] > 7 and intervalParemetrs[i][1] <= 20)):
+            thirdGroup.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[3], alpha=0.35, label="3-я группа (h<=0.3; w(7-20)" if len(thirdGroup) == 1 else "")
+        else:
+            fourthGroup.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[4], alpha=0.15, linestyle="dashed", label="4-я группа (h>0.3; w>20)" if len(fourthGroup) == 1 else "")
+
+
     plt.tight_layout()
     plt.legend() 
 
+    #=============================================================================
     # Параметры второй части графика
 
     plt.subplot(212).set_title("Анализ на минимумы")
@@ -58,31 +110,70 @@ def show_graphic(signalData):
     
     # Прорисовка интервалов
 
-    #=============================================================================
+    right = negativeProperties.get("right_ips")
+    left = negativeProperties.get("left_ips")
+
+    widthsParametr = negativeProperties.get("widths")
+    prominencesParametr = negativeProperties.get("prominences")
+    intervals = list(zip(left, right))
+    intervalParemetrs = list(zip(prominencesParametr, widthsParametr))
+
+    noiceIndex = []
+    firstGroup = []
+    secondGroup = []
+    thirdGroup = []
+    fourthGroup = []
+
+    for i in range(0, len(intervalParemetrs)):
+        if intervalParemetrs[i][0] <=0.025 and intervalParemetrs[i][1] <= 2:
+            noiceIndex.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[0], alpha=1, label="Потенциальный шум (h<=0.025; w<=2)" if len(noiceIndex) == 1 else "")
+
+        elif intervalParemetrs[i][0] <=0.025 and (intervalParemetrs[i][1] > 2 and intervalParemetrs[i][1] <= 3):
+            firstGroup.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[1], alpha=0.75, label="1-я группа (h<=0.025; w(2-3))" if len(firstGroup) == 1 else "")
+
+        elif ((intervalParemetrs[i][0] <=0.025) and (intervalParemetrs[i][1] > 3 and intervalParemetrs[i][1] <= 7)):
+            secondGroup.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[2], alpha=0.5, label="2-я группа (h<=0.025; w(3-7)" if len(secondGroup) == 1 else "")
+
+        elif ((intervalParemetrs[i][0] <=0.3) and (intervalParemetrs[i][1] > 7 and intervalParemetrs[i][1] <= 20)):
+            thirdGroup.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[3], alpha=0.35, label="3-я группа (h<=0.3; w(7-20)" if len(thirdGroup) == 1 else "")
+        else:
+            fourthGroup.append(i)
+            plt.axvspan(intervals[i][0], intervals[i][1], \
+                color=colorList[4], alpha=0.15, label="4-я группа (h>0.3; w>20)" if len(fourthGroup) == 1 else "")
 
     plt.tight_layout()
     plt.legend() 
     plt.show()
 
-    return positivePeaks, positiveProperties, negativePeaks, negativeProperties
-    
-###############################################################
 
 
 if __name__ == "__main__":
 
+    print("Данная программа выполняет поиск и классификацию участков функции")
+    print("В качестве примера выбран датасэт electrocardiogram библиотеки scipy")
+    a = int(input("Введите начало оцениваемого участка - "))
+    while a < 0:
+        a = int(input("Введите начало оцениваемого участка - "))
 
-    x = electrocardiogram()[0:300]
+    b = int(input("Введите конец оцениваемого участка - "))
+    while b < 0 or b < a:
+        b = int(input("Введите конец оцениваемого участка - "))
 
-    positivePeaks, positiveProperties, negativePeaks, negativeProperties = show_graphic(x)
+    x = electrocardiogram()[a:b]
 
-    print(len(positiveProperties.get("widths")))
+    header = str(a) + "-" + str(b)
 
-    right = positiveProperties.get("right_ips")
-    left = positiveProperties.get("left_ips")
+    positivePeaks, positiveProperties, negativePeaks, negativeProperties = get_grafic_parametr(x)
+    print("Обнаружено {} пиков в положительной области".format(len(positivePeaks)))
+    print("Обнаружено {} пиков в отрицательной области".format(len(negativePeaks)))
 
-    for i in range(0, len(right)):
-        print(left[i],"-", right[i])
-        print("----------------------------------")
+    show_graphic(positivePeaks, positiveProperties, negativePeaks, negativeProperties, header)
 
-    # positivePeaks, positiveProperties, negativePeaks, negativeProperties = show_graphic(x)
